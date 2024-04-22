be32686c604ff73ccaa86cd1b760f38aadfbd4c2
Jason Bekker <jasonbekker45@gmail.com>
2024-04-22 10:39:09 +0300

    added a few styles

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,52 +5,50 @@
 @section('title',$task->title)
 @section('content')
 
+<div class="mb-4">
+<a class="link" href="{{route('tasks.index')}}"> <- Back to all task</a>
+</div>
 
-<P>
-    
+<P class="mb-4 text-slate-700">
     {{$task->description}}
 </P>
 
 @if($task->long_description)
-    <p>{{ $task->long_description}}</p>
+    <p class="mb-4 text-slate-700" >{{ $task->long_description}}</p>
 @endif
 
-<p>{{ $task->created_at }}</p>
+<p class='mb-4 text-sm text-slate-500'>created {{ $task->created_at->diffForHumans() }}</p>
 
-<p>{{ $task->updated_at }}</p>
-<p>
+<p class='mb-4 text-sm text-slate-500'> updated {{ $task->updated_at->diffForHumans() }}</p>
+<p class="mb-4">
     @if($task->completed)
-    completed
+    <span  class='font-medium text-green-500' class>completed</span>
 
     @else
-    not completed
+   <span  class='font-medium text-red-500' class>not completed</span>
     @endif
 
 </p>
 
-<div>
-    <a href="{{route('tasks.edit', ['task'=>$task->id])}}">
+<div class="flex  gap-2">
+    <a class="btn" href="{{route('tasks.edit', ['task'=>$task->id])}}">
         Edit
     </a>
-</div>
 
-<div>
     <form  method="POST"  action="{{route('tasks.toggle-complete', ['task'=>$task])}}">
         @csrf
         @method('PUT')
-        <button type="submit">
+        <button type="submit" class="btn">
             Mark as {{$task->completed ?'not completed ' : ' completed '}} 
         </button>
 
 
     </form>
-</div>
 
-<div>
-    <form method="POST" action="{{route('tasks.destroy',['task'=>$task->id]) }}">
+    <form method="POST"  action="{{route('tasks.destroy',['task'=>$task->id]) }}">
         @csrf
         @method('DELETE')
-        <button type="submit">
+        <button type="submit" class="btn">
             Delete Task
         </button>
     </form>
